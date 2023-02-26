69c7dbeb6efec74156d55eefb81a6c064a939bff
Mohammad Toosi <m_dd@rogers.com>
2023-02-26 23:33:25 +0330

    chore: reset if user leave games running

--- src/pages/game/controller.ts
+++ src/pages/game/controller.ts
@@ -1,7 +1,20 @@
 import { gameStore } from "@/stores";
+import { useEffect } from "react";
 
 const useGameController = () => {
   const { status } = gameStore.useSelector();
+
+  useEffect(() => {
+    return () => {
+      if (status === "running-game") {
+        window.onbeforeunload = function () {
+          return "You are now running the game, are you sure?";
+        };
+
+        gameStore.changeStatus("setup-form");
+      }
+    };
+  }, [status]);
 
   return { status };
 };
