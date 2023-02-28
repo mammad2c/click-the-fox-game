5cf35818f62f5c9bf9640d1fe04cad5e4b31e744
Mohammad Toosi <m_dd@rogers.com>
2023-03-01 01:18:00 +0330

    chore: add api prefix route

--- src/server/router.ts
+++ src/server/router.ts
@@ -5,7 +5,7 @@ import { PhotosController } from "./controllers/photos/photos.controller";
 
 const router = (app: Express) => {
   // define your routes and map to your controllers
-  app.get("/photos", new PhotosController().index);
+  app.get("/api/photos", new PhotosController().index);
 };
 
 export { router };
