8b3981e6591dd92ddd53a0d0cb2186ad4f9c06cc
Mohammad Toosi <m_dd@rogers.com>
2023-03-02 02:12:59 +0330

    feat: add simple-grid ui component

--- src/client/ui/index.ts
+++ src/client/ui/index.ts
@@ -3,3 +3,4 @@ export { Text } from "./text/text";
 export { Button } from "./button/button";
 export { Container } from "./container/container";
 export { FormInput } from "./form-input/form-input";
+export { SimpleGrid } from "./simple-grid/simple-grid";
---
+++ src/client/ui/simple-grid/simple-grid.tsx
@@ -0,0 +1,5 @@
+import { SimpleGrid as UISimpleGrid } from "@chakra-ui/react";
+
+const SimpleGrid = UISimpleGrid;
+
+export { SimpleGrid };
