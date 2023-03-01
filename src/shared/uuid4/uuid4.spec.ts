1d53f1e6fc4856af1bbae8914eae9990df0c8acf
Mohammad Toosi <m_dd@rogers.com>
2023-03-01 13:32:15 +0330

    fix: better uuid testing

--- src/shared/uuid4/uuid4.spec.ts
+++ src/shared/uuid4/uuid4.spec.ts
@@ -1,6 +1,8 @@
 import { uuid4 } from "./uuid4";
 
-const uuid4Regex = RegExp(/^[a-z,0-9,-]{36,36}$/m);
+// grabbed from https://github.com/uuidjs/uuid/blob/main/src/regex.js
+const uuid4Regex =
+  /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
 
 describe("uuid4", () => {
   it("should generate valid uuid v4", () => {
