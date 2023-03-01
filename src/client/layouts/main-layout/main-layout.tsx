9edd2a3992a3da16c97e7497416ddd0ae9ba675a
Mohammad Toosi <m_dd@rogers.com>
2023-03-02 02:11:33 +0330

    fix: remove max-width from main-layout

--- src/client/layouts/main-layout/main-layout.tsx
+++ src/client/layouts/main-layout/main-layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <Box as="main">
-      <Container minHeight="100vh">
+      <Container minHeight="100vh" maxW="none">
         <Outlet />
       </Container>
     </Box>
