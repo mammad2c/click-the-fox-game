0d9050e4a92c9ffb7334c567ebe356edddbe26b8
Mohammad Toosi <m_dd@rogers.com>
2023-03-04 06:21:59 +0330

    chore: align center scoreboard page

--- src/client/pages/scoreboard/scoreboard.tsx
+++ src/client/pages/scoreboard/scoreboard.tsx
@@ -5,29 +5,38 @@ const Scoreboard = () => {
   const { table, headers, mounted, clearTable } = useScoreboardController();
 
   return (
-    <Container py={8} maxW="container.lg">
-      <Text textAlign="center" as="h2" fontSize="4xl" mb={4}>
-        Scoreboard
-      </Text>
-      {mounted && <Table headers={headers} items={table} />}
-      <Box display="flex" justifyContent="center" my={4}>
-        <Button mx={2} to="/">
-          To Welcome Screen
-        </Button>
-        <Button mx={2} to="/game">
-          Play
-        </Button>
-        <Button
-          mx={2}
-          backgroundColor="red"
-          onClick={clearTable}
-          color="white"
-          _hover={{ color: "black", opacity: 0.7 }}
-        >
-          Clear the table
-        </Button>
-      </Box>
-    </Container>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+      flexDirection="column"
+      width="100%"
+    >
+      <Container py={8} maxW="container.lg">
+        <Text textAlign="center" as="h2" fontSize="4xl" mb={4}>
+          Scoreboard
+        </Text>
+        {mounted && <Table headers={headers} items={table} />}
+        <Box display="flex" justifyContent="center" my={4}>
+          <Button mx={2} to="/">
+            To Welcome Screen
+          </Button>
+          <Button mx={2} to="/game">
+            Play
+          </Button>
+          <Button
+            mx={2}
+            backgroundColor="red"
+            onClick={clearTable}
+            color="white"
+            _hover={{ color: "black", opacity: 0.7 }}
+          >
+            Clear the table
+          </Button>
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
