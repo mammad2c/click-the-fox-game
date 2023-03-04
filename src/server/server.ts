971bdb932b7c573a8e1013b09ec96457ef0ae177
Mohammad Toosi <m_dd@rogers.com>
2023-03-05 02:13:06 +0330

    fix: production build

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 import fs from "fs";
 import path from "path";
 import { createServer as createViteServer } from "vite";
+import express from "express";
 import { PORT } from "../config";
 import { app } from "./app";
 
 async function createServer() {
+  const isProduction = process.env.NODE_ENV === "production";
+
   // Create Vite server in middleware mode and configure the app type as
   // 'custom', disabling Vite's own HTML serving logic so parent server
   // can take control
@@ -17,12 +20,23 @@ async function createServer() {
   // if you use your own express router (express.Router()), you should use router.use
   app.use(vite.middlewares);
 
+  if (isProduction) {
+    app.use(
+      "/assets",
+      express.static(path.join(__dirname, "../", "entry-client", "assets")),
+    );
+  }
+
   app.use("*", async (req, res, next) => {
     const url = req.originalUrl;
 
     try {
       // 1. Read index.html
-      let template = fs.readFileSync(path.resolve("index.html"), "utf-8");
+      const templatePath = isProduction
+        ? path.join(__dirname, "../", "entry-client", "index.html")
+        : "index.html";
+
+      let template = fs.readFileSync(path.resolve(templatePath), "utf-8");
 
       // 2. Apply Vite HTML transforms. This injects the Vite HMR client, and
       //    also applies HTML transforms from Vite plugins, e.g. global preambles
@@ -32,9 +46,12 @@ async function createServer() {
       // 3. Load the server entry. vite.ssrLoadModule automatically transforms
       //    your ESM source code to be usable in Node.js! There is no bundling
       //    required, and provides efficient invalidation similar to HMR.
-      const { render } = await vite.ssrLoadModule(
-        "/src/client/entry-server.tsx",
-      );
+
+      const entryServer = isProduction
+        ? path.join(__dirname, "../", "entry-server", "entry-server.mjs")
+        : "/src/client/entry-server.tsx";
+
+      const { render } = await vite.ssrLoadModule(entryServer);
 
       // 4. render the app HTML. This assumes entry-server.js's exported `render`
       //    function calls appropriate framework SSR APIs,
